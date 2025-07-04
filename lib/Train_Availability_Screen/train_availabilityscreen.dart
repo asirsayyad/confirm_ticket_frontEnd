9631e048804b9cc99d26a88237af787a68e0a942
Abdul Azim <termiz90095@gmail.com>
2025-07-04 18:47:28 +0530

    Your commit message

---
+++ lib/Train_Availability_Screen/train_availabilityscreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TrainAvailabilityscreen extends StatelessWidget {
+  const TrainAvailabilityscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold( 
+      appBar: AppBar(title: Text("TRAIN AVAILABILITY"),),
+    );
+  }
+}
